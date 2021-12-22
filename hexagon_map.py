b436af36f3e2fd364b95d5a6d2f278a00d511ba4
Alif <55590611+casualcoffeeaddict@users.noreply.github.com>
2021-12-22 17:56:46 +0000

    method ideas

---
+++ hexagon_map.py
@@ -0,0 +1,10 @@
+'''This will define the boundary of the hexagon maze for the given layout of the maze'''
+
+
+def hexagon_map(nCol, nRow):
+    '''This will make the map of the platform'''
+    pass
+
+def excluded_platforms():
+    '''This will define the areas of the map where the robots can not go as due to the pathfinding of the robot'''
+    pass
---
+++ main.py
@@ -0,0 +1,16 @@
+'''Main Maze program that will move the robots around'''
+
+# Check whether the intersection between the rings (list) are part of the target platform
+def get_path():
+    '''Constructs the path between the new and new and old positions'''
+    # get inner ring of platforms
+    # travel around middle rings
+    # Check whether the intersection between the rings (list) are part of the target platform
+    pass
+
+def main():
+    '''Main loop of code'''
+    pass
+
+if __name__ == '__main__':
+    main()
