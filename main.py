fc47b4df0e919b3fc2e9cfa4c859353b6531fd2a
Alif Aziz <alif.aziz@icloud.com>
2021-12-29 13:42:25 +0000

    Update

--- main.py
+++ main.py
@@ -6,19 +6,33 @@ def get_path():
 
     pass
 
-def intersection(robot1, robot2):
-    '''check the paths of the robots so that they dont intersect'''
+def intersection(platform_robot1, platform_robot2):
+    '''check the paths of the platform robots do not intersect'''
+    if :
+        # robot paths do not intersect
+        return True
+    else:
+        # robot paths do intersect
+        return False
+
+def reassign_robots():
+    '''Changes the names of the robots to work with the program'''
+    # animal_robot =
+    # platform_robot1 =
+    # platform_robot2 =
     pass
 
 def main():
     '''Main loop of code'''
-    # get the new locations (from the inner ring from the location of the animal's choice)
-    # send the robot from the inner ring to the outer ring
-    # get the path from location in the outer ring to the required location in the outer ring
-    # travel around middle rings to the desired location
-    # Check whether the intersection between the rings of both the paths that exist intersect
-    if intersection(robot1, robot2) == False:
-        # send the code to ssh
+    while animal_robot != goal_platform:
+        # get the new locations (from the inner ring from the location of the animal's choice)
+        # send the robot from the inner ring to the outer ring
+        # get the path from location in the outer ring to the required location in the outer ring
+        # travel around middle rings to the desired location
+        # Check whether the intersection between the rings of both the paths that exist intersect
+        if intersection(platform_robot1, platform_robot2) == False:
+            # send the code to ssh
+        # get input from the DLC reassignment
     pass
 
 if __name__ == '__main__':
