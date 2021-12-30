0ad879b0bb5371249ac284974a2040f77c834966
Alif Aziz <alif.aziz@icloud.com>
2021-12-30 14:51:47 +0000

    function to check if turn is possible added

--- hexagon_move.py
+++ hexagon_move.py
@@ -9,20 +9,32 @@ def valid_turn(turning_robot, robot1, robot2):
         return True
 
 
-def rel_turn(robot, clock, number):
-    '''robot is the robotbeing turned, clock is either clockwise or anticlockwise, number is 1 - 6'''
+def rel_turn(turning_robot, rotation, robot1, robot2):
+    '''robot is the robot being turned, number is 1 - 6'''
     # check if turn is valid
     if valid_turn(turning_robot, robot1, robot2):
-        # Perform turn
+        rotation = rotation % 6
+        if rotation == 0:
+            print('No rotation')
+        elif 0 < rotation < 3:
+            print('Turn right')
+        elif rotation == 3:
+            print('Turn behind')
+        elif 3 < rotation < 6:
+            print('Turn left')
+        print(rotation)
     else:
         print('turn is not valid: robots consecutive to turning robot')
 
+def ssh_turn():
+    '''send turn command to robot program'''
+    pass
 
 def test():
     turning_robot = Hex(0, 0, 0)
     robot1 = Hex(2, -2, 0)
     robot2 = Hex(-2, 2, 0)
-    print(valid_turn(turning_robot, robot1, robot2))
+    print(rel_turn(turning_robot, -10, robot1, robot2))
 
 if __name__ == '__main__':
     test()
