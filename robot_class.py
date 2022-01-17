89ceed16e12bcf5692d9fc75999e047e70c6d733
Alif <55590611+casualcoffeeaddict@users.noreply.github.com>
2022-01-17 14:11:41 +0000

    added getting relative position of platforms

--- robot_class.py
+++ robot_class.py
@@ -11,12 +11,12 @@ class PlatformRobot():
         self.z = z
         self.rotation = self.dimension_dict[rotation]
 
-    def rel_rotation_change(self, change):
+    def change_rotation(self, change):
         '''change is between 0-2'''
         self.rotation += change
         return self.rotation // 3
 
-    def move_position(self, dimension, step):
+    def change_position(self, dimension, step):
         rotation_check = self.dimension_dict[dimension]
         if self.rotation != dimension:
             change = self.dimension_dict[dimension] - self.rotation
@@ -36,44 +36,19 @@ class PlatformRobot():
         else:
             print('select correct dimension: x, y, z')
 
-
-
-    # def find_path(self, fx, fy, fz):
-    #     # move to outer ring
-    #     outer_ring_move = self.move_to_outer_ring()
-    #     # Precession
-    #     clockwise_steps = len(self.clockwise_step(True, ))
-    #     anticlockwise_steps = len(self.clockwise_step(False, ))
-    #     # decide on clockwise or anticlockwise movement
-    #     if clockwise_steps < anticlockwise_steps:
-    #         direction_of_precess = True
-    #     elif clockwise_steps > anticlockwise_steps:
-    #         direction_of_precess = False
-    #     elif clockwise_steps == anticlockwise_steps:
-    #     ## The direction must be opposite to the other robots
-    #         precess_steps = int(final_rel_position - self.rel_position)
-    #     # Final position needs its own rel position to the animal platform
-    #     # From this the precess steps can be calculated
-    #     # Moving clockwise around 4 is the same as anticlockwise 2
-    #     # Therefore clockwise
-    #     precesssion_values = self.precession(clockwise=direction_of_precess, steps=precess_steps)
-    #     inner_ring_move = self.move_to_inner_ring()
-
-        # combine move list
-        # return [*outer_ring_move, *precesssion_values, *inner_ring_move]
-
-
 class AnimalRobot(PlatformRobot):
     '''Class for the platform with the animal on it'''
-    def __init__(self):
-        pass
+    def __init__(self, x, y, z, rotation):
+        super().__init__(x, y, z, rotation)
 
 
 class NonAnimalRobot(PlatformRobot):
     '''Class for the platform without the animal on it'''
-    def __init__(self, AnimalRobot, rel_position):
+    def __init__(self, x, y, z, rotation, AnimalRobot, rel_position, ):
+        super().__init__(x, y, z, rotation)
         # for encoding position relative to animal robot
-        self.rel_position = rel_position  # position around the hexagonal ring of the animal robot: between 0-5 where 0 = NE, 1 = E ...
+        self.rel_position_dict = {'NE':0, 'E':1, 'SE':2, 'SW':3, 'W':4, 'NW':5}
+        self.rel_position = self.rel_position_dict[rel_position]  # position around the hexagonal ring of the animal robot:
         self.animalrobot = AnimalRobot
         # for precession method
         self.clockwise_dim = ['x', 'y', 'z', 'x', 'y', 'z']
@@ -85,19 +60,20 @@ class NonAnimalRobot(PlatformRobot):
         self.outer_ring_steps = [1, 1, 1, -1, -1, -1]
         self.inner_ring_steps = [-1, -1, -1, 1, 1, 1]
 
-    def position(self):
-        return self.animalrobot
+    def get_relative_animal_vector(self):
+        return (self.x-self.animalrobot.x,
+                self.y-self.animalrobot.y,
+                self.z-self.animalrobot.z)
 
-    def find_ainimal_platform(self):
-        '''takes position of animal robot, converts it to start position, a number in the range 0-5, which are the
-        verticies of a hexagon'''
-        self.rel_position = start
+    def get_relative_position(self):
+        vector_to_cardinal = {(1,0,-1): 0, (1,-1,0): 1, (0,-1,1):2, (-1,0,1):3, (-1, 1, 0):4, (0,1,-1):5}
+        return vector_to_cardinal[self.get_relative_animal_vector()]
 
     def move_to_outer_ring(self):
         '''move to outer ring'''
         outer_ring_move = []
         outer_ring_move.append(
-            self.move(self.ring_dim[self.rel_position], self.outer_ring_steps[self.rel_position])
+            self.change_position(self.ring_dim[self.rel_position], self.outer_ring_steps[self.rel_position])
         )
         return outer_ring_move
 
@@ -105,7 +81,7 @@ class NonAnimalRobot(PlatformRobot):
         '''move to inner ring'''
         inner_ring_move = []
         inner_ring_move.append(
-            self.move(self.ring_dim[self.rel_position], self.inner_ring_steps[self.rel_position])
+            self.change_position(self.ring_dim[self.rel_position], self.inner_ring_steps[self.rel_position])
         )
         return inner_ring_move
 
@@ -116,7 +92,7 @@ class NonAnimalRobot(PlatformRobot):
             # precess clockwise
             for i in range(0, steps):
                 precession_values.append(
-                    self.move_position(self.clockwise_dim[self.rel_position], self.clockwise_step[self.rel_position])
+                    self.change_position(self.clockwise_dim[self.rel_position], self.clockwise_step[self.rel_position])
                 )
                 self.rel_position += 1
                 if self.rel_position == 6:
@@ -126,7 +102,7 @@ class NonAnimalRobot(PlatformRobot):
             # precess anticlockwise
             for i in range(0, steps):
                 precession_values.append(
-                    self.move_position(self.anticlockwise_dim[self.rel_position], self.anticlockwise_step[self.rel_position])
+                    self.change_position(self.anticlockwise_dim[self.rel_position], self.anticlockwise_step[self.rel_position])
                 )
                 self.rel_position += 1
                 if self.rel_position == 6:
@@ -136,15 +112,16 @@ class NonAnimalRobot(PlatformRobot):
 
 
 class AnimalGoal(PlatformRobot):
-    '''Class with the goal on it'''
-    def __init__(self):
-        pass
+    '''Class of platform robot with the goal on it'''
+    def __init__(self, x, y, z, rotation):
+        super().__init__(x, y, z, rotation)
+
 
 def main():
-    ar = AnimalRobot(0,0,0,'x',)
-    nar = NonAnimalRobot()
+    ar = AnimalRobot(0,0,0,'x')
+    nar = NonAnimalRobot(0,-1,1,'x', ar, 'NE')
 
-    print(nar.position())
+    print(nar.get_relative_position())
 
 if __name__ == '__main__':
     main()
